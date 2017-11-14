8882a30f691d08de9424e9ad96a483d433c5ccbe
rasul93 <rasulchik93@gmail.com>
2017-11-14 18:36:20 +0200

    my first comment

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$int = 1;
+$string = '23';
+
+$array = [1, 3, 5, 6, "name" => "Vadim", "sername" => "Petrov", 7, 8, 9, 'users' => ["Vadim", "Alex", "Ruslan"]];
+
+
+//for ($i = 0; $i < 15; $i++) {
+//    echo "<pre>";
+//    var_dump($array[$i]);
+//    echo "</pre>";
+//}
+//foreach ($array as $key => $value) {
+//    if (is_array($value)) {
+//        foreach ($value as $key => $value) {
+//            var_dump($value);
+//        }
+//    }
+//}
+//while ($i < 5) {
+//    $count = $i++;
+//    echo "<pre>";
+//    var_dump($i);
+//    echo "</pre>";
+//}
+
+//var_dump($int);
+//var_dump($string);
+//var_dump($array);
+
+?>
